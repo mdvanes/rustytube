eb3660ccf7630e8b68b03f3eadc7750d58a5a355
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-06-16 14:52:18 +0200

    add serde_json

--- client/src/list_items.rs
+++ client/src/list_items.rs
@@ -2,48 +2,27 @@
 use egui::Ui;
 
 pub fn show_list_items(ui: &mut Ui) {
-    ui.label("Foo");
+    ui.label("Foo3");
 
     let request = ehttp::Request::get("https://jsonplaceholder.typicode.com/posts");
     ehttp::fetch(request, move |result: ehttp::Result<ehttp::Response>| {
-        // println!("Status code: {:?}", result.unwrap().status);
-        // if let Ok(response) = result {
-        //     if let Ok(posts_json) = response.text() {
-        //         if let Ok(posts) = serde_json::from_str::<Vec<serde_json::Value>>(&posts_json) {
-        //             ui.heading("Posts:");
-        //             for post in posts {
-        //                 if let Some(title) = post.get("title").and_then(|t| t.as_str()) {
-        //                     ui.label(title);
-        //                 }
-        //             }
-        //         } else {
-        //             ui.label("Failed to parse posts JSON");
-        //         }
-        //     } else {
-        //         ui.label("Failed to read posts response");
-        //     }
-        // } else {
-        //     ui.label("Failed to fetch posts");
-        // }
+        if let Ok(response) = result {
+            if let Ok(posts_json) = std::str::from_utf8(&response.bytes) {
+                println!("x");
+                if let Ok(posts) = serde_json::from_str::<Vec<serde_json::Value>>(posts_json) {
+                    for post in posts {
+                        if let Some(title) = post.get("title").and_then(|t| t.as_str()) {
+                            println!("{}", title);
+                        }
+                    }
+                } else {
+                    println!("Failed to parse posts JSON");
+                }
+            } else {
+                println!("Failed to read posts response");
+            }
+        } else {
+            println!("Failed to fetch posts");
+        }
     });
-
-    // if let Ok(response) = ureq::get("https://jsonplaceholder.typicode.com/posts").call() {
-    //     // if let Ok(posts_json) = response.into_string() {
-    //     //     let posts_result: Result<Vec<serde_json::Value>, _> = serde_json::from_str(&posts_json);
-    //     //     if let Ok(posts) = posts_result {
-    //     //         ui.heading("Posts:");
-    //     //         for post in posts {
-    //     //             if let Some(title) = post.get("title").and_then(|t| t.as_str()) {
-    //     //                 ui.label(title);
-    //     //             }
-    //     //         }
-    //     //     } else {
-    //     //         ui.label("Failed to parse posts JSON");
-    //     //     }
-    //     // } else {
-    //     //     ui.label("Failed to read posts response");
-    //     // }
-    // } else {
-    //     ui.label("Failed to fetch posts");
-    // }
 }
