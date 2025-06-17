fdb2542ee3dd9e8bffea7b33fc710acc0e168d4c
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-06-17 16:56:19 +0200

    move server index.html to static dir

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,8 @@
-use std::{sync::Arc};
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::Logger, post, web};
 use actix_web_lab::extract::Path;
 use log::info;
 use std::env;
+use std::sync::Arc;
 
 mod broadcast;
 use self::broadcast::Broadcaster;
@@ -17,18 +17,9 @@ async fn posts() -> impl Responder {
         .body("[{\"title\": \"Hello, world!\"}]")
 }
 
-// #[post("api/echo")]
-// async fn echo(req_body: String) -> impl Responder {
-//     HttpResponse::Ok().body(req_body)
-// }
-
-// async fn manual_hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hey there!")
-// }
-
 #[get("/")]
 async fn index() -> impl Responder {
-    web::Html::new(include_str!("index.html").to_owned())
+    web::Html::new(include_str!("static/index.html").to_owned())
 }
 
 // test by running: curl -X POST 127.0.0.1:8081/broadcast/my_message
@@ -50,17 +41,17 @@ async fn broadcast_msg(
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
-    
+
     let data = Broadcaster::create();
-    
+
     let port: u16 = env::var("PORT")
         .ok()
         .and_then(|s| s.parse().ok())
         .unwrap_or(8081);
-    
+
     info!("server running on port {}", port);
     println!("Server running on {}", port);
-    
+
     HttpServer::new(move || {
         use actix_cors::Cors;
         App::new()
@@ -68,7 +59,7 @@ async fn main() -> std::io::Result<()> {
                 Cors::default()
                     .allow_any_origin()
                     .allow_any_method()
-                    .allow_any_header()
+                    .allow_any_header(),
             )
             .app_data(web::Data::from(Arc::clone(&data)))
             .service(index)
