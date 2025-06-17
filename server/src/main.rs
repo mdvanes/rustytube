72f6f4938373cfb088056607083576faafde6aac
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-06-17 13:08:00 +0200

    add server sent event example

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,11 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::{sync::Arc};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
+use actix_web_lab::extract::Path;
 use log::info;
 use std::env;
+
+mod broadcast;
+use self::broadcast::Broadcaster;
 
 #[get("/api/posts")]
 async fn posts() -> impl Responder {
@@ -12,31 +17,63 @@ async fn posts() -> impl Responder {
         .body("[{\"title\": \"Hello, world!\"}]")
 }
 
-#[post("api/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+// #[post("api/echo")]
+// async fn echo(req_body: String) -> impl Responder {
+//     HttpResponse::Ok().body(req_body)
+// }
+
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
+
+#[get("/")]
+async fn index() -> impl Responder {
+    web::Html::new(include_str!("index.html").to_owned())
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+// test by running: curl -X POST 127.0.0.1:8081/broadcast/my_message
+#[get("/events")]
+async fn event_stream(broadcaster: web::Data<Broadcaster>) -> impl Responder {
+    broadcaster.new_client().await
+}
+
+#[post("/broadcast/{msg}")]
+async fn broadcast_msg(
+    broadcaster: web::Data<Broadcaster>,
+    Path((msg,)): Path<(String,)>,
+) -> impl Responder {
+    broadcaster.broadcast(&msg).await;
+    HttpResponse::Ok().body("msg sent")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
+    
+    let data = Broadcaster::create();
+    
     let port: u16 = env::var("PORT")
         .ok()
         .and_then(|s| s.parse().ok())
         .unwrap_or(8081);
+    
     info!("server running on port {}", port);
     println!("Server running on {}", port);
-    HttpServer::new(|| {
+    
+    HttpServer::new(move || {
+        // App::new()
+        //     .service(posts)
+        //     .service(echo)
+        //     .route("/api/hey", web::get().to(manual_hello))
         App::new()
-            .service(posts)
-            .service(echo)
-            .route("/api/hey", web::get().to(manual_hello))
+            .app_data(web::Data::from(Arc::clone(&data)))
+            .service(index)
+            .service(event_stream)
+            .service(broadcast_msg)
+            .wrap(Logger::default())
     })
     .bind(("127.0.0.1", port))?
+    .workers(2)
     .run()
     .await
 }
